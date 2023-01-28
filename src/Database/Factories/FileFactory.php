ad1f6ad183299f574e6b50b3c0e3ac94b7432db1
Seth Phat <phattranminh96@gmail.com>
2023-01-28 15:58:49 +0700

    Added tests

--- src/Database/Factories/FileFactory.php
+++ src/Database/Factories/FileFactory.php
@@ -3,6 +3,7 @@
 namespace SaasReady\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use SaasReady\Models\Currency;
 use SaasReady\Models\File;
 
 class FileFactory extends Factory
@@ -12,7 +13,15 @@ class FileFactory extends Factory
     public function definition(): array
     {
         return [
-            '' => '',
+            'model_id' => fn () => Currency::factory()->create(),
+            'model_type' => Currency::class,
+            'category' => $this->faker->creditCardType(),
+            'filename' => $this->faker->userName() . $this->faker->fileExtension(),
+            'original_filename' => $this->faker->userName() . $this->faker->fileExtension(),
+            'size' => random_int(1000, 999999),
+            'path' => $this->faker->filePath(),
+            'mime_type' => $this->faker->mimeType(),
+            'source' => 'test',
         ];
     }
 }
